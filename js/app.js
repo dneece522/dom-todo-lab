bee9429d22b15d3db9d392c0a61e625c5952817c
Drew Neece <dneece5@aggienetwork.com>
2022-12-07 20:48:16 -0600

    complete working event listener for button

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ const inpEl = document.getElementById('inp')
 const btnEl = document.getElementById('submit-button')
 const ulEl = document.getElementById('todo-list')
 
+// CODE BELOW //
+//Event listener that listens when the button is pushed, creates a list item, sets the list item to whatever text is in the input, and appends the list item to the unordered list
+btnEl.addEventListener('click', function(evt) {
+  const li = document.createElement('li')
+  li.textContent = inpEl.value
+  ulEl.appendChild(li)
+})
